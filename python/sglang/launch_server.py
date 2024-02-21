b7ff57eeadecd5bda3c8d2737c842437b6ffd937
liuquan <18646313696@163.com>
2024-02-21 14:50:23 +0800

    update mp_start_method

--- python/sglang/launch_server.py
+++ python/sglang/launch_server.py
@@ -1,8 +1,11 @@
 import argparse
+import multiprocessing as mp
+
 
 from sglang.srt.server import ServerArgs, launch_server
 
 if __name__ == "__main__":
+    mp.set_start_method("spawn")
     parser = argparse.ArgumentParser()
     ServerArgs.add_cli_args(parser)
     args = parser.parse_args()
